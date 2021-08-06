0013ea601e6040f2c4d77c606f35841927dcdc45
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-06 18:21:48 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,9 @@
+// for (i=0;i<=10;i++){
+// const number= Math.random()*15;
+// const output= Math.ceil(number);
+// console.log(output);
+// }
+
+const number= 5.12;
+const output=Math.floor(number);
+console.log(output);
