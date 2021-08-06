04afe5e30646903356fa9062667c0fa6ca122825
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-06 23:06:58 +0600

    math Function Done

--- math.js
+++ math.js
@@ -1,9 +1,9 @@
-// for (i=0;i<=10;i++){
-// const number= Math.random()*15;
-// const output= Math.ceil(number);
-// console.log(output);
-// }
+for (i=0;i<=10;i++){
+const number= Math.random()*15;
+const output= Math.ceil(number);
+console.log(output);
+}
 
 const number= 5.12;
-const output=Math.floor(number);
+const output=Math.round(number);
 console.log(output);
