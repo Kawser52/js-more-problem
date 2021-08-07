0379f99d4d3c10e720bd23c311fe2537c296a40e
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-07 11:43:48 +0600

    largest and Lowest Number is done

---
+++ lowest-number.js
@@ -0,0 +1,10 @@
+// Lowest Number printing
+let largest=14;
+let numbers=[14, 54, 14, 47];
+for (i=0;i<numbers.length;i++){
+  let element=numbers[i];
+  if(element<largest){
+      largest= element;
+  }
+}
+console.log(largest);
