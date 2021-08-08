16d90202fcf8f0eeb03898d92ca5f334f5431b7d
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-08 11:15:15 +0600

    Febonacchi series complete

--- febonacchi.js
+++ febonacchi.js
@@ -1,5 +1,21 @@
-let fibbo= [6, 8];
-for (i=2; i<=10; i++){
-     fibbo[i] =fibbo[i-1]+fibbo[i-2];
+// let fibbo= [6, 8];
+// for (i=2; i<=10; i++){
+//      fibbo[i] =fibbo[i-1]+fibbo[i-2];
+// }
+// console.log(fibbo);
+
+function febonacchiSeries(num){
+    let fibbo= [6, 8];
+    if (typeof num != 'number'){
+        return "Please give me Number ";
+    }
+    if (num<2){
+        return 'Please give me a postivie number';
+    }
+    for (i=2; i<=num; i++){
+        fibbo[i] =fibbo[i-1]+fibbo[i-2];
+    }
+    console.log(fibbo);
 }
-console.log(fibbo);
+const number= febonacchiSeries(-5);
+console.log(number);
---
+++ recursive.js
@@ -0,0 +1,11 @@
+/* function recursiveFunction(n){
+    if (n==0){
+        return 0;
+    }
+    if (n==1){
+        return 1;
+    }
+    return recursiveFunction(n-1)+ recursiveFunction(n-2)
+}
+const element= recursiveFunction(8);
+console.log(element); */
